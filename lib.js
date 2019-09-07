bf713f255256e00a4edb313faef92b9d65d5d388
kensuke goto <utogoken2@yahoo.co.jp>
2019-09-07 16:57:46 +0900

    yeah!!

--- lib.js
+++ lib.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const testJoin = (a, b) => {
   console.log("iam groovy!");
+  console.log("iam groovy!");
+  console.log("iam groovy!");
+  console.log("iam groovy!");
+  console.log("iam groovy!");
   return path.join(a, b);
 };
 
